660c2fd35c838a4bd0e3afb806eac1209576f7dd
theDirtyDeeds <ghost054rus@mail.ru>
2024-05-22 16:22:05 +0700

    Add files via upload

---
+++ PracticalWork 5.1/PracticalWork 5.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace PracticalWork_5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            SplitText(GetText());
+        }
+
+        static string GetText()
+        {
+
+            Console.WriteLine("Введите предложение");
+
+            string text = Console.ReadLine();
+
+            return text;
+        }
+
+        static string[] SplitText(string text)
+        {
+            string[] words = text.Split(' ');
+
+            foreach (string w in words)
+            {
+                Console.WriteLine(w);
+            }
+            return words;
+        }
+    }
+}
---
+++ PracticalWork 5.1/PracticalWork 5.2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PracticalWork_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string getText = Console.ReadLine();
+
+            Reverse(getText);
+        }
+        static string[] SplitText(string text)
+        {
+            string[] words = text.Split(" ");
+
+            foreach (string w in words)
+            {
+                Console.WriteLine(w);
+            }
+            return words;
+        }
+
+        static string Reverse(string reverseText)
+        {
+
+            for (int i = SplitText(reverseText).Length; i >= 0; i--)
+            {
+                Console.Write(reverseText[i]);
+            }
+            return reverseText;
+        }
+    }
+}
